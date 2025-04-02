fc45bb8ac3a1a9e2f0361ccc404aa4a4916154bb
MarianaValle22 <marianavamo@unisabana.edu.co>
2025-04-02 14:26:45 -0500

    First commit for client-flow

--- app/(app)/client/_layout.tsx
+++ app/(app)/client/_layout.tsx
@@ -7,6 +7,9 @@ export default function RootLayout() {
       screenOptions={{ headerShown: false }} 
       >
         <Stack.Screen name="index"/>
+        <Stack.Screen name="car"/>
+        <Stack.Screen name="success"/>
+        <Stack.Screen name="orderStatus"/>
       </Stack>
   )
 }
